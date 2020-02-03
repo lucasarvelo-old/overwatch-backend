30ce2914a5b4601666313d24bdeec73af143e135
Lucas Arvelo <lucasarvelo@gmail.com>
2020-02-02 20:16:39 -0500

    Add and Config Helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,6 +16,15 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(
+  helmet({
+    hidePoweredBy: { setTo: 'PHP 4.2.0' },
+    frameguard: { action: 'sameorigin' },
+    noCache: {},
+    referrerPolicy: { policy: 'same-origin' },
+  })
+);
 
 app.use(logger('dev'));
 app.use(express.json());
